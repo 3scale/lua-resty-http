dacef04fa49fd44d6a93d4b1cccc4087baac65cb
An Tran <tkan145@gmail.com>
2026-07-27 12:14:51 +1000

    feat(ssl): add support to set custom CA store
    This commit add `ssl_trusted_store` option when connection to a SSL
    endpoint with a custom trusted CA store.

--- lib/resty/http_connect.lua
+++ lib/resty/http_connect.lua
@@ -3,6 +3,7 @@ local ngx_re_gmatch = ngx.re.gmatch
 local ngx_re_sub = ngx.re.sub
 local ngx_re_find = ngx.re.find
 local ngx_log = ngx.log
+local ngx_md5 = ngx.md5
 local ngx_WARN = ngx.WARN
 local ngx_DEBUG = ngx.DEBUG
 local to_hex = require("resty.string").to_hex
@@ -53,6 +54,11 @@ client:connect {
     ssl_client_cert = nil,
     ssl_client_priv_key = nil,
 
+    -- Custom trusted CA store (cdata `X509_STORE*`), passed to
+    -- `tcpsock:settrustedstore`. Requires a cosocket build with
+    -- `settrustedstore` support.
+    ssl_trusted_store = nil,
+
     proxy_opts,             -- proxy opts, defaults to global proxy options
 }
 ]]
@@ -81,6 +87,7 @@ local function connect(self, options)
     -- ssl settings
     local ssl, ssl_reused_session, ssl_server_name
     local ssl_verify, ssl_send_status_req, ssl_client_cert, ssl_client_priv_key
+    local ssl_trusted_store
     if request_scheme == "https" then
         ssl = true
         ssl_reused_session = options.ssl_reused_session
@@ -92,6 +99,7 @@ local function connect(self, options)
         end
         ssl_client_cert = options.ssl_client_cert
         ssl_client_priv_key = options.ssl_client_priv_key
+        ssl_trusted_store = options.ssl_trusted_store
     end
 
     -- proxy related settings
@@ -233,6 +241,22 @@ local function connect(self, options)
         cert_hash = to_hex(cert_hash) -- convert to hex so that it's printable
     end
 
+    -- Validate ssl_trusted_store
+    if ssl and ssl_trusted_store then
+      if type(ssl_trusted_store) ~= "cdata" then
+        return nil, "bad ssl_trusted_store: cdata expected, got " .. type(ssl_trusted_store)
+      end
+
+      if type(sock.setclientcert) ~= "function" then
+        return nil, "cannot use ssl_trusted_store without settrustedstore support"
+      end
+    end
+
+    local ssl_trusted_store_id
+    if ssl_trusted_store then
+        ssl_trusted_store_id = ngx_md5(tostring(ssl_trusted_store))
+    end
+
     -- construct a poolname unique within proxy and ssl info
     if not poolname then
         poolname = (request_scheme or "")
@@ -244,6 +268,7 @@ local function connect(self, options)
                    .. ":" .. (proxy_uri or "")
                    .. ":" .. (request_scheme == "https" and proxy_authorization or "")
                    .. ":" .. (cert_hash or "")
+                   .. ":" .. tostring(ssl_trusted_store_id or "")
         -- in the above we only add the 'proxy_authorization' as part of the poolname
         -- when the request is https. Because in that case the CONNECT request (which
         -- carries the authorization header) is part of the connect procedure, whereas
@@ -315,8 +340,18 @@ local function connect(self, options)
           else
               ok, err = sock:setclientcert(ssl_client_cert, ssl_client_priv_key)
               if not ok then
+                  self:close()
                   return nil, "could not set client certificate: " .. err
               end
+          end
+        end
+
+        -- Custom CA trusted store support
+        if ssl_trusted_store then
+          ok, err = sock:settrustedstore(ssl_trusted_store)
+          if not ok then
+              self:close()
+              return nil, "could not set trusted store: " .. err
           end
         end
 
